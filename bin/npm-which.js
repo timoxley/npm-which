e0706142d06cee3e98ea81d744f598d43b878b72
Tim Oxley <secoif@gmail.com>
2014-04-21 08:14:31 +0800

    Add -c option to match which(1).

--- bin/npm-which.js
+++ bin/npm-which.js
@@ -9,14 +9,23 @@ var npmWhich = require('../')
 
 program
   .version(pkg.version)
-  .option('-a, --all')
-  .option('-c', 'No output, just return 0 if any of the executables are found, or 1 if none are found.')
+  .option('-c, --silent', 'No output, just return 0 if any of the executables are found, or 1 if none are found.')
   .usage('<command>')
   .parse(process.argv)
 
 if (!program.args.length) return program.help()
 
 var cmd = program.args[0]
+
+if (program.silent) {
+  try {
+    npmWhich.sync(cmd)
+    process.exit(0)
+  } catch (e) {
+    if (!e.message.match('not found:')) throw e
+    process.exit(1)
+  }
+}
 
 try {
   console.log(npmWhich.sync(cmd))
